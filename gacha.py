ac16a220d530f124ae942a1dc790fbefa1fb6111
txrTanzi <txrtanzi@gmail.com>
2021-06-15 14:50:23 +0800

    Standard gacha

--- gacha.py
+++ gacha.py
@@ -2,23 +2,34 @@ from op import Operator
 
 
 class Gacha:
-    def __init__(self, start_time, end_time, filename, pickup_op, *, shop_op=None, link=None):
+    def __init__(self, start_time, end_time, filename, pickup_op, *, shop_op=None, link=None, name=None, comment1=None,
+                 comment2=None):
         if shop_op is None:
             shop_op = []
         if link is None:
             link = ""
+        if name is None:
+            name = ""
+        if comment1 is None:
+            comment1 = ""
+        if comment2 is None:
+            comment2 = ""
         self.start_time = start_time
         self.end_time = end_time
         self.filename = filename
         self.pickup_op = pickup_op
         self.shop_op = shop_op
         self.link = link
+        self.name = name
+        self.comment1 = comment1
+        self.comment2 = comment2
 
         for op in shop_op:
             pickup_op.remove(op)
 
     def show(self):
         print(self.filename)
+        print(self.name)
         print(f"start time: {self.start_time}")
         print(f"end time: {self.end_time}")
         print(f"shop:")
@@ -29,3 +40,7 @@ class Gacha:
             print(f"{op},")
         for op in self.pickup_op:
             print(f"{op},")
+        if self.comment1 != "":
+            print(self.comment1)
+        if self.comment2 != "":
+            print(self.comment2)
--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import asyncio
 import time
 from textwrap import dedent
 
+import httpx
 from lxml import html
 import yaml
 
 from aiowikibot import Bot
 from gacha import Gacha
 from op import Operator
+
+with open('const.yaml', encoding='utf-8') as f:
+    const = yaml.load(f, Loader=yaml.BaseLoader)
 
 
 async def main():
@@ -17,26 +21,50 @@ async def main():
     bot = Bot(setting['username'], setting['password'], setting['api_url'])
     await bot.login_wiki()
     tasks = []
-    # await generate_wikitext(bot)
-    text = await generate_wikitext(bot, range=(0, 18))
-    tasks.append(bot.write_wiki('User:Txrtanzi/sandbox/gacha/2019', text, 'Edited by bot.', bot=True, minor=False))
-    text = await generate_wikitext(bot, range=(18, 44))
-    tasks.append(bot.write_wiki('User:Txrtanzi/sandbox/gacha/2020', text, 'Edited by bot.', bot=True, minor=False))
-    text = await generate_wikitext(bot, range=(44, 55))
-    tasks.append(bot.write_wiki('User:Txrtanzi/sandbox/gacha/2021', text, 'Edited by bot.', bot=True, minor=False))
-    # tasks = [update_op(bot), update_gacha(bot)]
-    await asyncio.gather(*tasks)
+    selection = ''
+    while selection != 'exit':
+        tasks.clear()
+        selection = input('Update or write:')
+        if selection == 'update':
+            # tasks.append(update_op(bot))
+            tasks.append(update_gacha(bot))
+            try:
+                await asyncio.gather(*tasks)
+            except httpx.ReadTimeout:
+                print("Tine Out")
+        elif selection == 'write':
+            text = await generate_standard_gacha(bot)
+            tasks.append(
+                bot.write_wiki('用户:Txrtanzi/sandbox/gacha/full', text, 'Edited by bot.', bot=True, minor=False))
+            await asyncio.gather(*tasks)
+        elif selection == 'print':
+            await print_data()
     await bot.close()
 
 
-async def generate_wikitext(bot, *, range=None):
+async def print_data():
+    gacha = input("rotate or standard:")
+    if gacha == 'rotate':
+        with open('gacha_rotate.yaml') as g:
+            gacha_list = yaml.unsafe_load(g)
+            for gacha in gacha_list:
+                gacha.show()
+    elif gacha == 'standard':
+        with open('gacha_standard.yaml') as g:
+            gacha_list = yaml.unsafe_load(g)
+            for gacha in gacha_list:
+                gacha.show()
+
+
+async def generate_standard_gacha(bot, *, range=None):
+    """Generate wikitext for standard gacha page"""
     '''Range of gacha to display in list'''
     gacha_range = range
     '''String to return'''
     text = []
     print('out')
     '''Load gacha and operator data'''
-    with open('gacha.yaml') as g:
+    with open('gacha_standard.yaml') as g:
         gacha_list = yaml.unsafe_load(g)
     with open('operator.yaml') as op:
         op_list = yaml.unsafe_load(op)
@@ -45,21 +73,82 @@ async def generate_wikitext(bot, *, range=None):
     for op in op_list:
         op_index[op.name] = op.star
     print('out')
-    rotate_title = """\
-        {| class="wikitable mw-collapsible fullline logo" style="width:100%; text-align:center; white-space:normal; display:table;"
-        ! style="width:5%" |序号
-        ! style="width:25%" |寻访页面
-        ! style="width:20%" |开启时间
-        ! style="width:25%" |特定干员（6星）
-        ! style="width:25%" |特定干员（5星）"""
+    rotate_title = const['standard_gacha_title']
 
     text.append(dedent(rotate_title))
     '''process range'''
     if gacha_range is None:
         gacha_range = (0, len(gacha_list))
-    elif len(range) == 1:
+    elif type(range) is int:
         gacha_range = (range, len(gacha_list))
+    print(gacha_range)
+    t_list = gacha_list[gacha_range[0]: gacha_range[1]]
+    t_list.reverse()
+    '''generate table'''
+    for gacha in t_list:
+        time_array = time.localtime(int(gacha.start_time))
+        s_time = time.strftime("%Y-%m-%d %H:%M", time_array)
+        time_array = time.localtime(int(gacha.end_time))
+        e_time = time.strftime("%Y-%m-%d %H:%M", time_array)
+        table = f"""
+            |-
+            |[[{gacha.filename}|400px|link={gacha.link}]]<br/>[[{gacha.link}|{gacha.name}]]
+            |{s_time}~<br/>{e_time}"""
+        text.append(dedent(table))
+        text.append('\n|')
+        for op in gacha.shop_op:
+            if op_index[op] == 5:
+                text.append(f"{{{{干员头像|{op}|shop=1}}}}")
+        for op in gacha.pickup_op:
+            if op_index[op] == 5:
+                text.append(f"{{{{干员头像|{op}}}}}")
+        if gacha.comment1 != "":
+            text.append(f"\n※{gacha.comment1}")
+        text.append('\n|')
+        for op in gacha.shop_op:
+            if op_index[op] < 5:
+                text.append(f"{{{{干员头像|{op}|shop=1}}}}")
+        for op in gacha.pickup_op:
+            if op_index[op] < 5:
+                text.append(f"{{{{干员头像|{op}}}}}")
+        if gacha.comment2 != "":
+            text.append(f"\n※{gacha.comment2}")
+        text.append('')
+    text.append('\n|}')
 
+    return ''.join(text)
+
+
+async def generate_special_gacha(bot, *, range=None):
+    return None
+
+
+async def generate_rotate_gacha(bot, *, range=None):
+    """Generate wikitext for rotate gacha page"""
+    '''Range of gacha to display in list'''
+    gacha_range = range
+    '''String to return'''
+    text = []
+    print('out')
+    '''Load gacha and operator data'''
+    with open('gacha_rotate.yaml') as g:
+        gacha_list = yaml.unsafe_load(g)
+    with open('operator.yaml') as op:
+        op_list = yaml.unsafe_load(op)
+    ''''dump operator name and star into a dict for query'''
+    op_index = {}
+    for op in op_list:
+        op_index[op.name] = op.star
+    print('out')
+    rotate_title = const['rotate_gacha_title']
+
+    text.append(dedent(rotate_title))
+    '''process range'''
+    if gacha_range is None:
+        gacha_range = (0, len(gacha_list))
+    elif type(range) is int:
+        gacha_range = (range, len(gacha_list))
+    print(gacha_range)
     t_list = gacha_list[gacha_range[0]: gacha_range[1]]
     t_list.reverse()
     '''generate table'''
@@ -96,9 +185,7 @@ async def generate_wikitext(bot, *, range=None):
 
 async def update_op(bot):
     print('op')
-    with open('setting.yaml', encoding='utf-8') as s:
-        setting = yaml.load(s, Loader=yaml.BaseLoader)
-    ask_string = setting['ask_op']
+    ask_string = const['ask_op']
     tasks = [bot.ask(ask_string)]
     results = await asyncio.gather(*tasks)
     print('op')
@@ -110,17 +197,16 @@ async def update_op(bot):
                 int(data['printouts']['稀有度'][0]),
                 data['printouts']['干员序号'][0]
             ))
-    with open('operator.yaml', 'w') as f:
-        yaml.dump(op, f)
+    with open('operator.yaml', 'w') as op_file:
+        yaml.dump(op, op_file)
     print("Operator data updated.")
 
 
 async def update_gacha(bot):
     print("gacha")
-    with open('setting.yaml', encoding='utf-8') as s:
-        setting = yaml.load(s, Loader=yaml.BaseLoader)
-    ask_string = setting['ask_gacha']
-    tasks = [bot.ask(ask_string)]
+    ask_string = [const['ask_rotate_gacha'], const['ask_standard_gacha']]
+    tasks = [bot.ask(ask_string[0]), bot.ask(ask_string[1])]
+    print(ask_string[1])
     results = await asyncio.gather(*tasks)
     print("gacha")
     gacha = []
@@ -134,8 +220,34 @@ async def update_gacha(bot):
                 shop_op=data['printouts']['商店兑换干员'],
                 link=data['printouts']['卡池名'][0]
             ))
-    with open('gacha.yaml', 'w') as f:
-        yaml.dump(gacha, f)
+    with open('gacha_rotate.yaml', 'w') as rotate_file:
+        yaml.dump(gacha, rotate_file)
+    gacha = []
+    for result in results[1]['results']:
+        for file, data in result.items():
+            g_result = Gacha(
+                data['printouts']['寻访开启时间cn'][0]['timestamp'],
+                data['printouts']['寻访关闭时间cn'][0]['timestamp'],
+                file,
+                data['printouts']['出率提升干员'],
+                link=data['printouts']['卡池名'][0],
+                name=data['printouts']['寻访名cn'][0]
+            )
+            try:
+                g_result.comment1 = data['printouts']['备注1'][0]
+            except IndexError:
+                pass
+            else:
+                print('Found comment 1')
+            try:
+                g_result.comment2 = data['printouts']['备注2'][0]
+            except IndexError:
+                pass
+            else:
+                print('Found comment 2')
+            gacha.append(g_result)
+    with open('gacha_standard.yaml', 'w') as standard_file:
+        yaml.dump(gacha, standard_file)
     print("Gacha data updated.")
 
 
