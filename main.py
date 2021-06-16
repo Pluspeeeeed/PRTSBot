2a47b6e5bf18958d947edfbceef24336889086d6
txrTanzi <txrtanzi@gmail.com>
2021-06-17 00:08:16 +0800

    All in one

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ async def main():
             except httpx.ReadTimeout:
                 print("Time Out")
         elif selection == 'write':
-            # text = await generate_standard_and_special_gacha(bot, range=33)
-            # tasks.append(
-            #     bot.write_wiki('用户:Txrtanzi/卡池一览', text, 'Edited by bot.', bot=True, minor=False))
             '''卡池一览/限时寻访'''
             text = ['==非标准寻访==\n', await generate_gacha(category='special'),
                     '\n==标准寻访==\n', await generate_gacha()]
@@ -41,12 +38,12 @@ async def main():
             tasks.append(
                 bot.write_wiki('用户:Txrtanzi/卡池一览/限时寻访', page_text, 'Edited by bot.', bot=True, minor=False))
             '''卡池一览'''
-            # text = [const['gacha_header'], await generate_standard_and_special_gacha(range=33),
-            #         '\n==常驻标准寻访==\n', await generate_rotate_gacha(range=44), const['seo_text']]
-            # page_text = ''.join(text)
-            # print(text)
-            # tasks.append(
-            #     bot.write_wiki('用户:Txrtanzi/卡池一览', page_text, 'Edited by bot.', bot=True, minor=False))
+            text = [const['gacha_header'], await generate_gacha(range=33, category='overview'),
+                    '\n==常驻标准寻访==\n', await generate_gacha(range=44, category='rotate'), const['seo_text']]
+            page_text = ''.join(text)
+            tasks.append(
+                bot.write_wiki('用户:Txrtanzi/卡池一览', page_text, 'Edited by bot.', bot=True, minor=False))
+            '''卡池一览/常驻标准寻访'''
             text = await generate_gacha(category='rotate')
             tasks.append(
                 bot.write_wiki('用户:Txrtanzi/卡池一览/常驻标准寻访', text, 'Edited by bot.', bot=True, minor=False))
@@ -85,9 +82,14 @@ async def generate_gacha(*, range=None, category='standard'):
     print('out')
 
     gacha_list = []
-    for gacha in full_list:
-        if gacha.category == category:
-            gacha_list.append(gacha)
+    if category != 'overview':
+        for gacha in full_list:
+            if gacha.category == category:
+                gacha_list.append(gacha)
+    else:
+        for gacha in full_list:
+            if gacha.category != 'rotate':
+                gacha_list.append(gacha)
 
     if category == "rotate":
         gacha_title = const['rotate_gacha_title']
@@ -164,91 +166,6 @@ async def generate_gacha(*, range=None, category='standard'):
     return ''.join(text)
 
 
-async def generate_standard_and_special_gacha(*, range=None):
-    """Generate wikitext for gacha overview page"""
-    '''Range of gacha to display in list'''
-    gacha_range = range
-    '''String to return'''
-    text = []
-    print('out')
-    '''Load gacha and operator data'''
-    with open('gacha_standard_and_special.yaml') as g:
-        gacha_list = yaml.unsafe_load(g)
-    with open('operator.yaml') as op:
-        op_dict = yaml.unsafe_load(op)
-    print('out')
-    gacha_title = const['standard_gacha_title']
-
-    text.append(dedent(gacha_title))
-    '''process range'''
-    if gacha_range is None:
-        gacha_range = (0, len(gacha_list))
-    elif type(range) is int:
-        gacha_range = (range, len(gacha_list))
-    print(gacha_range)
-    t_list = gacha_list[gacha_range[0]: gacha_range[1]]
-    t_list.reverse()
-    '''generate table'''
-    for gacha in t_list:
-        time_array = time.localtime(int(gacha.start_time))
-        s_time = time.strftime("%Y-%m-%d %H:%M", time_array)
-        time_array = time.localtime(int(gacha.end_time))
-        e_time = time.strftime("%Y-%m-%d %H:%M", time_array)
-        time_text = ''
-        if gacha.series == '':
-            table = f"""
-                |-
-                |[[{gacha.filename}|400px|link={gacha.link}]]<br/>[[{gacha.link}|{gacha.name}]]
-                |{s_time}~<br/>{e_time}"""
-        else:
-            table = f"""
-                |-
-                |[[{gacha.filename}|400px|link={gacha.link}]]<br/>[[{gacha.link}|【限定寻访·{gacha.series}】{gacha.name}]]
-                |{s_time}~<br/>{e_time}"""
-        text.append(dedent(table))
-        text.append('\n|')
-        for op in gacha.shop_op:
-            if op_dict[op]['star'] == 5:
-                text.append(f"{{{{干员头像|{op}|shop=1}}}}")
-        for op in gacha.pickup_op:
-            if op_dict[op]['star'] == 5:
-                if op_dict[op]['obtain'] == '限定寻访':
-                    text.append(f"{{{{干员头像|{op}|limited=1}}}}")
-                    if op_dict[op]['limit_time'] != '':
-                        time_array = time.localtime(int(op_dict[op]['limit_time']))
-                        limit_time = time.strftime("%Y-%m-%d", time_array)
-                        time_text = f'''
-                        ※限定干员【{op}】加入【{gacha.series}】系列限定寻访的时间为{limit_time}'''
-                else:
-                    text.append(f"{{{{干员头像|{op}}}}}")
-        text.append(dedent(time_text))
-        time_text = ''
-        if gacha.comment1 != "":
-            text.append(f"\n※{gacha.comment1}")
-        text.append('\n|')
-        for op in gacha.shop_op:
-            if op_dict[op]['star'] < 5:
-                text.append(f"{{{{干员头像|{op}|shop=1}}}}")
-        for op in gacha.pickup_op:
-            if op_dict[op]['star'] < 5:
-                if op_dict[op]['obtain'] == '限定寻访':
-                    text.append(f"{{{{干员头像|{op}|limited=1}}}}")
-                    if op_dict[op]['limit_time'] != '':
-                        time_array = time.localtime(int(op_dict[op]['limit_time']))
-                        limit_time = time.strftime("%Y-%m-%d", time_array)
-                        time_text = f'''
-                        ※限定干员【{op}】加入【{gacha.series}】系列限定寻访的时间为{limit_time}'''
-                else:
-                    text.append(f"{{{{干员头像|{op}}}}}")
-        text.append(dedent(time_text))
-        if gacha.comment2 != "":
-            text.append(f"\n※{gacha.comment2}")
-        text.append('')
-    text.append('''\n|}''')
-
-    return ''.join(text)
-
-
 async def update_op(bot):
     print('op')
     ask_string = const['ask_op']
@@ -299,9 +216,6 @@ async def update_gacha(bot):
                 link=data['printouts']['卡池名'][0],
                 category='rotate'
             ))
-    # with open('gacha_rotate.yaml', 'w') as rotate_file:
-    #     yaml.dump(gacha, rotate_file)
-    # gacha.clear()
     for result in results[1]['results']:
         for file, data in result.items():
             g_result = Gacha(
@@ -325,9 +239,6 @@ async def update_gacha(bot):
             else:
                 print('Found comment 2')
             gacha.append(g_result)
-    # with open('gacha_standard.yaml', 'w') as standard_file:
-    #     yaml.dump(gacha, standard_file)
-    # gacha.clear()
     for result in results[2]['results']:
         for file, data in result.items():
             g_result = Gacha(
@@ -358,13 +269,13 @@ async def update_gacha(bot):
             else:
                 print('Found limit series')
             gacha.append(g_result)
+            gacha.sort(key=lambda t_gacha: t_gacha.start_time)
     with open('gacha.yaml', 'w') as gacha_file:
         yaml.dump(gacha, gacha_file)
     print("Gacha data updated.")
 
 
 if __name__ == "__main__":
-    # asyncio.set_event_loop_policy(None)
     loop = asyncio.get_event_loop()
     start = time.perf_counter()
     loop.run_until_complete(main())
